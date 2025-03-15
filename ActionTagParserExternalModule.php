038905027fe7995be163ac2b8725b544dc785906
Günther Rezniczek <g.rezniczek@gmail.com>
2025-03-15 15:36:12 +0100

    Refine bench

--- ActionTagParserExternalModule.php
+++ ActionTagParserExternalModule.php
@@ -52,37 +52,55 @@ class ActionTagParserExternalModule extends AbstractExternalModule {
     function benchmark() {
 
         $n = max(intval($_GET["n"]), 1);
+        $timings = [];
 
         print "<h5>Timing ($n iterations)</h5>";
         print "<p>Set the number of iterations as GET parameter '<i>n</i>'.</p>";
 
-        $start = microtime(true);
-
+        
         for ($i = 0; $i < $n; $i++) {
+            $start = microtime(true);
             $parser_tags = ActionTagParser::getActionTags(null, null, null, null, false, $i);
+            $end = microtime(true);
+            $timings["Parser"][] = $end-$start;
         }
-
-        $end = microtime(true);
-        print "<p>Parser: ".round(($end-$start) * 1000, 2)." µs</p>";
         
-        $start = microtime(true);
-
+        
         for ($i = 0; $i < $n; $i++) {
+            $start = microtime(true);
             $parser_tags = ActionTagParser::getActionTags(null, null, null, null, true, $i);
+            $end = microtime(true);
+            $timings["Parser (Optimized)"][] = $end-$start;
         }
-
-        $end = microtime(true);
-        print "<p>Parser (Optimized): ".round(($end-$start) * 1000, 2)." µs</p>";
-        
-        $start = microtime(true);
         
         for ($i = 0; $i < $n; $i++) {
+            $start = microtime(true);
             $helper_tags = ActionTagHelper::getActionTags(null, null, null, null, $i);
+            $end = microtime(true);
+            $timings["Helper"][] = $end-$start;
         }
-        
-        $end = microtime(true);
-        print "<p>Helper: ".round(($end-$start) * 1000, 2)." µs</p>";
-        
+
+        // Calculat averange and standard deviation
+        $avg = function($arr) {
+            return array_sum($arr) / count($arr);
+        };
+        $sd = function($arr) use ($avg) {
+            $mean = $avg($arr);
+            $sum = 0;
+            foreach ($arr as $value) {
+                $sum += pow($value - $mean, 2);
+            }
+            return sqrt($sum / count($arr));
+        };
+
+        print "<table class=\"table table-sm table-responsive\" style=\"width: 350px;\">";
+        print "<tr><th>Method</th><th>Mean<br>(µs)</th><th>Std. Dev.<br>(µs)</th></tr>";
+        foreach ($timings as $key => $value) {
+            $mean = $avg($timings[$key]);
+            $std = $sd($timings[$key]);
+            print "<tr><td>$key</td><td>".round($mean * 1000,2)."</td><td>".round($std * 1000, 2)."</td></tr>";
+        }
+        print "</table>";
 
         $print = function($tags) {
             foreach ($tags as $tag => $fields) {
@@ -95,9 +113,9 @@ class ActionTagParserExternalModule extends AbstractExternalModule {
             }
         };
 
-        print "<hr>";
         print "<h5>Parser:</h5>";
         $print($parser_tags);
+        print "<hr>";
         print "<h5>Helper:</h5>";
         $print($helper_tags);
     }
